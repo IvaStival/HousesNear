e01c837bee37d3f11cbfa5e36978f8f1ff2faacd
iva stival <ivastival@gmail.com>
2022-07-08 17:38:32 -0300

    Create some variables on silver layer, like: Ciy, district, street. To eache amenities make a columns (get_dummies), and adjust anothers variables.

--- data/silver/code/silver_manip.py
+++ data/silver/code/silver_manip.py
@@ -1,14 +1,18 @@
 # %%
+from curses.ascii import isalpha
+import re
 import pandas as pd
-import math
-
+import numpy
+import unidecode
+numpy.set_printoptions(threshold=sys.maxsize)
+pd.options.display.max_colwidth = 100
 # %%
 df = pd.read_csv("../../bronze/data/houses_full_load.csv")
 df.head()
 # %%
 
 # REMOVE USELESS COLUMNS     
-df = df.drop(['Unnamed: 0', 'id'], axis=1)
+df = df.drop(['id'], axis=1)
 
 # DROP USELESS ROWS
 df = df.drop(df.loc[df["h_url"] == "url"].index)
@@ -34,14 +38,30 @@ df_edited = df_edited.drop("batrooms", axis=1)
 df_edited["garage"] = df_edited["garage"].apply(lambda x : x.split(" ")[0].replace("--", "0").split("-")[0])
 
 ## VALUE
-df_edited["h_value"] = df_edited["h_value"].apply(lambda x : int(x.split(" ")[1].split("\n")[0].replace(".", "")))
-# REMOVE THE RENT APARTMENTS
-df_edited = df_edited.loc[df_edited["h_value"] < 15000]
+df_edited["h_value"] = df_edited["h_value"].apply(lambda x : x.split(" ")[-1].replace(".", ""))
+df_edited["h_value"] = df_edited["h_value"].apply(lambda x : x if(x.isnumeric()) else 0)
 
 ## CONDOMINIUM
 df_edited.loc[df_edited["condominium"].isna(), "condominium"] = '0'
 df_edited["condominium"] = df_edited["condominium"].apply(lambda x : x.split(" ")[-1].replace(".", ""))
 
+## AMENITIES
+df_edited["amenities"].fillna("Not")
+df_edited["amenities"] = df_edited["amenities"].apply(lambda x : unidecode.unidecode(str(x).lower()).replace(" ", "_").replace("...", ""))
+df_edited = pd.concat([df_edited, df_edited["amenities"].str.get_dummies(sep="\n")], axis=1)
+df_edited = df_edited.drop("amenities", axis=1)
+
+## BAIRROS
+address_norm = (df_edited["h_address"].str.lower().str.normalize("NFKD")
+                                                .str.encode("ascii", errors="ignore")
+                                                .str.decode("utf8"))
+df_edited["bairros"] = pd.DataFrame(address_norm)["h_address"].apply(lambda x : re.findall("(^[a-z]+[\s*[a-z]*]*),\s\D|-\s([a-z]+[\s*[a-z]*]*),",x)[0][1])
+
+## CITY 
+df_edited["city"] = pd.DataFrame(address_norm)["h_address"].apply(lambda x : re.findall(", ([a-z]+[\s*[a-z]*]*) -",x)[0])
+
+## STREET
+df_edited["street"] = pd.DataFrame(address_norm)["h_address"].apply(lambda x : re.findall(" +([a-z]+[\s*[a-z]*]*),* \d* *-",x)[0].replace(" ","_"))
 # %%
-df_edited["condominium"].unique()
+df_edited
 # %%
